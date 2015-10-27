daa545f0454ab1401d5989573f3a1b12ece135df
alexli-al <alex.li@missionsky.com>
2015-10-27 22:58:49 +0800

    something wrong in forEach()

--- todo.js
+++ todo.js
@@ -5,14 +5,17 @@ todoApp.controller("TodoListController",function(){
 	var todoList = this;
 	todoList.test="Alex";
 	todoList.todos = [
-		{text : "learn angular",done:true},
-		{text : "build an angular app",done:false}
+		{text : "learn angular",done : true},
+		{text : "build an angular app",done : false},
+		{text : "build my site with MEAN",done : false}
 	];
 
 
 	//remaining
 	todoList.remaining = function(){
 		var count = 0;
+
+		//something wrong there 
 		angular.forEach(todoList.todos , function(todo){
 			count + = todo.done ? 0 : 1;
 		});
